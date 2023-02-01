47c0c900d8a00a0367c793fba92fd28584a11c6c
artem-loban <artem.loban@exsportia.com>
2023-02-01 14:00:32 +0200

    first

---
+++ src/components/tree/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { people } from '../../people/tree';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_tm3u4ZwVbMvlwZ-Zpcb1DN-ioAxQDM8",
+  authDomain: "gene-tree-test.firebaseapp.com",
+  projectId: "gene-tree-test",
+  storageBucket: "gene-tree-test.appspot.com",
+  messagingSenderId: "820519603620",
+  appId: "1:820519603620:web:a9ee492eda6ea320854531",
+  measurementId: "G-PE5X9S87RJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+const setMember = async () => {
+  const membersCollection = collection(firestore, 'members');
+  const membersSnap = await getDocs(membersCollection);
+  return console.log(membersSnap)
+}
+
+const Tree = () => {
+  return <button onClick={() => setMember()}>asdasd</button>
+};
+
+export default Tree;
---
+++ src/people/tree.js
@@ -0,0 +1,24 @@
+export const people = [
+  {
+    id: '0001',
+    name: 'Artem',
+    surname: 'Loban',
+    parents: [
+      {
+        id: '0002',
+      }
+    ],
+    children: [],
+  },
+  {
+    id: '0002',
+    parents: [],
+    name: 'Andrey',
+    surname: 'Loban',
+    children: [
+      {
+        id: '0001',
+      }
+    ]
+  }
+]
